7628d5d9dbf72da87ecad73b5381e2e82015f1cb
Brenno Farias <38961716+fariasbrenno@users.noreply.github.com>
2023-12-10 23:13:17 -0300

    Iniciando repositório

---
+++ index.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="_css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <title>Vídeo Aulas</title>
+</head>
+<body>
+  <!--Header,menu,nav-->
+  <header>
+    <nav class="navbar has-shadow" role="navigation" aria-label="main navigation" id="nav-menu">
+      <div class="navbar-brand">
+        <a href="#" id="teste">
+          <img class="image is-64x64"src="_img/logo.png" alt="Logo" id="logo">
+        </a>
+      </div>
+      
+      <div class="navbar-end">
+        <a class="navbar-item is-tab" href="#">
+          <i class="fa-brands fa-whatsapp has-text-success"></i>
+          <span>Comercial</span>
+        </a>
+        <a class="navbar-item is-tab" href="#">
+          <i class="fa-brands fa-whatsapp has-text-success"></i>
+          <span>Suporte Técnico</span>
+        </a>
+      </div>
+    </nav>
+  </header>
+  <!--Main-->
+  <main>
+    <section class="container"> <!--Seção primeiros passos-->
+      <h1 class="title is-3 mt-6">Comece por aqui</h1>
+      <div class="columns">
+        <div class="column is-3">
+          <figure class="image">
+            <img src="https://bulma.io/images/placeholders/128x128.png" alt="">
+          </figure>
+          <h1 class="title is-6">Como acessar o painel de Streamin e visão geral</h1>
+          <p>Nesta aula você aprenderá acessar seu painel de streaming, além de ter uma explicação geral sobre as funcionalidades do seu painel</p>
+        </div>
+        <div class="column is-3">
+          <figure class="image">
+            <img src="https://bulma.io/images/placeholders/128x128.png" alt="">
+          </figure>
+          <h1 class="title is-6">Como acessar o painel de Streamin e visão geral</h1>
+          <p>Nesta aula você aprenderá acessar seu painel de streaming, além de ter uma explicação geral sobre as funcionalidades do seu painel</p>
+        </div>
+      </div>
+    </section>
+    <!--Seção playlists-video asulas-->
+    <section class="container">
+      <h1 class="title is-4 mt-5">Playlists - Video Aulas</h1>
+      <div class="columns">
+
+        <div class="column">
+          <h1 class="title is-6 has-text-centered">Playlist 01 - Iniciante</h1>
+          <div class="card">
+            <div class="card-image">
+              <figure class="image is-4by3">
+                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+              </figure>
+            </div>
+            <div class="content">
+              <p>Nesta playlist você vai aprender Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius facere accusantium optio harum adipisci a sunt, laborum assumenda ipsum odio libero aut quibusdam, praesentium doloremque animi, sapiente illo dolores eligendi!</p>
+            </div>
+            <div class="buttons is-centered">
+              <a href="#" class="button is-link is-half">
+                Acessar
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div class="column">
+          <h1 class="title is-6 has-text-centered">Playlist 02 - Intermediário</h1>
+          <div class="card">
+            <div class="card-image">
+              <figure class="image is-4by3">
+                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+              </figure>
+            </div>
+            <div class="content">
+              <p>Nesta playlist você vai aprender Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius facere accusantium optio harum adipisci a sunt, laborum assumenda ipsum odio libero aut quibusdam, praesentium doloremque animi, sapiente illo dolores eligendi!</p>
+            </div>
+            <div class="buttons is-centered">
+              <a href="#" class="button is-link is-half">
+                Acessar
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div class="column">
+          <h1 class="title is-6 has-text-centered">Playlist 03 - Avançado</h1>
+          <div class="card">
+            <div class="card-image">
+              <figure class="image is-4by3">
+                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+              </figure>
+            </div>
+            <div class="content">
+              <p>Nesta playlist você vai aprender Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius facere accusantium optio harum adipisci a sunt, laborum assumenda ipsum odio libero aut quibusdam, praesentium doloremque animi, sapiente illo dolores eligendi!</p>
+            </div>
+            <div class="buttons is-centered">
+              <a href="#" class="button is-link is-half">
+                Acessar
+              </a>
+            </div>
+          </div>
+        </div>
+           
+      </div>
+    </section>
+    <!--Seção de ferramentas-->
+    <section class="container mt-6">
+      Sessão de ferramentas
+    </section>
+    <!--Modal-->
+    <div class="modal">
+      <div class="modal-background ">
+      </div>
+        <div class="modal-content ">
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/rC6r1ne-BZ0?si=ZcRZVd1sy5WhX7zw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <span class="icon">
+            <i class="fa-solid fa-circle-xmark fa-2xl has-text-danger"></i>
+          </span>
+        </div>
+        
+    </div>
+  </main>
+  <footer class="footer">
+    Rodapé
+  </footer>
+</body>
+<script src="_js/script.js"></script>
+</html>
